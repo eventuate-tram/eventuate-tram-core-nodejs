43fbf84963c30917b1899a34c346e8ea9b470f5d
Chris Richardson <chris@chrisrichardson.net>
2019-10-11 00:27:38 -0700

    Fixed env

---
+++ .circleci/setenv-circle-ci.sh
@@ -0,0 +1,11 @@
+
+# Host DNS name doesnt resolve in Docker alpine images
+
+export DOCKER_HOST_IP=$(hostname -I | sed -e s/ .*//g)
+export TERM=dumb
+
+export EVENTUATE_TRAM_MYSQL_HOST=$DOCKER_HOST_IP
+export EVENTUATE_TRAM_MYSQL_PORT=3306
+export EVENTUATE_TRAM_MYSQL_DATABASE=eventuate
+export EVENTUATE_TRAM_MYSQL_USERNAME=mysqluser
+export EVENTUATE_TRAM_MYSQL_PASSWORD=mysqlpw
