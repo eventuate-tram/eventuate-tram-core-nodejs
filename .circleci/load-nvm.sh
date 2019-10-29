3617b983403683a0bdf2cdc6aa72a773b5b619a7
Chris Richardson <chris@chrisrichardson.net>
2019-10-29 14:33:03 -0700

    Upgraded CircleCI NodeJS version

--- .circleci/load-nvm.sh
+++ .circleci/load-nvm.sh
@@ -1,6 +1,2 @@
 export NVM_DIR=/opt/circleci/.nvm
-ls -lt /opt/circleci/.nvm
-ls -lt /opt/circleci/.nvm/nvm.sh
-more /opt/circleci/.nvm/nvm.sh
-which nvm
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
